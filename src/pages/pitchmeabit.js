0afca1461b23bd94a826a30bdcbf3ff5c7f2a1c7
Stefano Yushinski <syushinski@gmail.com>
2020-07-23 01:04:41 -0400

    added a new page

---
+++ src/pages/pitchmeabit.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const PitchMeABitPage = () => (
+  <Layout>
+    <SEO title="Pitch me a bit" />
+    <div style={{maxWidth: 600, width: 600}}>
+      <h1>Pitch me a bit</h1>
+      <p>Here's where you'll be able to write a one or two liner bit pitch. I have no where for you to get it to me at the moment, but think about some "guy whos" or "what ifs". </p>
+    </div>
+  </Layout>
+)
+
+export default PitchMeABitPage
